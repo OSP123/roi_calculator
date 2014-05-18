80ba1c57e1a83b29e11e1f25b363f4069f1ec60d
OSP123 <omar@getsmartify.com>
2014-05-17 17:09:53 -0700

    generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RoiCalculator::Application.config.secret_key_base = 'd3ab67a6d387a6f7f68ff98aa4ace582a807070fc419f05990df542b0a045d24c4288b56370842421ded6c19a5aec6ca447601a405ce7e7c7a4d02a9ae3b0737'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RoiCalculator::Application.config.session_store :cookie_store, key: '_roi_calculator_session'
