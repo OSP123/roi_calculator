7cc05384df925f53f6343dea70a1dfc0696f7617
OSP123 <omar@getsmartify.com>
2014-05-20 17:26:17 -0700

    added more htmla nd removed p tags

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,17 @@
+var actions_drop_down;
+var roi_defined_container;
+var roi_per_action_container;
+$(document).ready(function(){ 
+
+	actions_drop_down = $('#actions_drop_down');
+	roi_defined_container = $('#roi_per_action_container');
+	roi_per_action_container = $('#roi_per_action_container');
+  $('#add_action').click(function(){ 
+    (actions_drop_down.val());
+    var value = actions_drop_down.val();
+  	$('#' + value + '_text').show( "fold", 1000 );
+  	$('#' + value + '_defined').show( "fold", 1000 );
+  	$('#' + value + '_calculated').show( "fold", 1000 );
+  });
+
+});
